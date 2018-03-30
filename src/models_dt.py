d0c3d657e7955d89b8d81a5261d752c3c0f09f72
Drew Thayer <drewthayer@Drews-MacBook-Pro.local>
2018-03-30 14:15:37 -0600

    more figs

--- src/models_dt.py
+++ src/models_dt.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from data import *
 from plotroc import plot_roc_curve
 from bagging import bagging
+from knn import knn
 # imports
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
@@ -31,16 +32,19 @@ if __name__ == '__main__':
             ('logistic', LogisticRegression())
             ])
     pipe_logistic.fit(X_train, y_train)
-    print(pipe_logistic.score(X_test, y_test))
 
-    acc = cross_val_score(pipe_logistic,X_test, y_test, cv=5, scoring='accuracy')
-    precision = cross_val_score(pipe_logistic,X_test, y_test, cv=5, scoring='precision')
-    print('accuracy = {}'.format(np.mean(acc)))
-    print('precision = {}'.format(np.mean(precision)))
+    y_hat = pipe_logistic.predict(X_test)
+    score_accuracy = accuracy_score(y_test, y_hat)
+    score_precision = precision_score(y_test, y_hat)
+    print('accuracy = {}'.format(score_accuracy))
+    print('precision = {}'.format(score_precision))
 
     coefs = pipe_logistic.named_steps['logistic'].coef_
 
     print(coefs)
+
+    # knn classifier
+    knn = knn(X_train, X_test, y_train, y_test)
 
     # decision tree classifier
     dtc = DecisionTreeClassifier(max_depth=3,)
@@ -53,13 +57,13 @@ if __name__ == '__main__':
     dtc_acc = np.mean(cross_val_score(dtc, X_train, y_train, scoring='accuracy', cv=5))
     dtc_test_prec = precision_score(y_test, y_preds_dtc_bin)
     dtc_test_acc = accuracy_score(y_test, y_preds_dtc_bin)
-    print("The cross validated precision score is {:0.3}".format(dtc_prec))
-    print("The cross validated accuracy score is {:0.3}".format(dtc_acc))
-    print("The test precision score is {:0.3}".format(dtc_test_prec))
-    print("The test accuracy score is {:0.3}".format(dtc_test_acc))
+    print("DTC cross validated precision score is {:0.3}".format(dtc_prec))
+    print("DTC cross validated accuracy score is {:0.3}".format(dtc_acc))
+    print("DTC test precision score is {:0.3}".format(dtc_test_prec))
+    print("DTC test accuracy score is {:0.3}".format(dtc_test_acc))
 
     # bagging classifier
-    bagc = bagging(X_train, y_train, X_test, y_test)
+    bagc = bagging(X_train, X_test, y_train, y_test)
 
     # adaboost classifier
     ada = AdaBoostClassifier()
@@ -82,18 +86,18 @@ if __name__ == '__main__':
 
     # gradient boost grid search
     #%% grid search
-    '''grid search to find best params for gradient boost classifier '''
-    gbc_grid = {'learning_rate': np.linspace(0.2,0.8,4),
-                     'max_depth': [1,2,4,8],
-                     'min_samples_leaf': [2, 4, 6, 8],
-                     'max_features': ['sqrt', 'log2', None],
-                     'n_estimators': [100, 150, 200, 250, 300]}
-
-    gbc_gridsearch = GridSearchCV(GradientBoostingClassifier(),
-                        gbc_grid,
-                        n_jobs=-1,
-                        verbose=True,
-                        scoring='precision')
+    # '''grid search to find best params for gradient boost classifier '''
+    # gbc_grid = {'learning_rate': np.linspace(0.2,0.8,4),
+    #                  'max_depth': [1,2,4,8],
+    #                  'min_samples_leaf': [2, 4, 6, 8],
+    #                  'max_features': ['sqrt', 'log2', None],
+    #                  'n_estimators': [100, 150, 200, 250, 300]}
+    #
+    # gbc_gridsearch = GridSearchCV(GradientBoostingClassifier(),
+    #                     gbc_grid,
+    #                     n_jobs=-1,
+    #                     verbose=True,
+    #                     scoring='precision')
     #svc_gridsearch.fit(X_train, y_train)
     #print("best gbc parameters:", svc_gridsearch.best_params_)
     # # roc curve for logistic
@@ -106,8 +110,8 @@ if __name__ == '__main__':
 
     # plot muliple models on roc curve
     logistic_mod = pipe_logistic.named_steps['logistic']
-    models = [logistic_mod, dtc, bagc, ada, gbc]
-    model_names = ['logistic', 'decision tree', 'bagging', 'AdaBoost', 'Gradient Boost']
+    models = [logistic_mod, knn, dtc, bagc, ada, gbc]
+    model_names = ['logistic', 'knn', 'decision tree', 'bagging', 'AdaBoost', 'Gradient Boost']
     colors = ['b','k','m','g','r','c']
     plt.figure()
     for idx, model in enumerate(models):
